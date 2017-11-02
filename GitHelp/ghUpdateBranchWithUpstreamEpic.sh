7883143aaaa169b3a85a482d18471483822e49e0
Steven-D-Stamps <Steven.D.Stamps@ESPN.com>
2017-11-02 13:57:21 -0400

    new branch from epic

--- GitHelp/ghUpdateBranchWithUpstreamEpic.sh
+++ GitHelp/ghUpdateBranchWithUpstreamEpic.sh
@@ -3,14 +3,15 @@
 # alias = ghUBUE
 
 if [ ! $# -eq 1 ]; then
-  printf "\nUsage: ghUBUE JIRA_number\n"
-  printf "  Update current Branch with Upstream Epic branch.\n\n"
-  exit
+    printf "\nUsage: ghUBUE JIRA_number\n"
+    printf "  Update current Branch with Upstream Epic branch.\n\n"
+    exit
 fi
 
 UPSTREAM_BRANCH=`$GITHELP_HOME/ghParseEpicBranch.sh $1`
 if [ $? -ne 0 ] ; then
-  exit 1;
+    printf "\n    ERROR: Argument is not a JIRA number.\n\n"
+    exit 1;
 fi
 
 $GITHELP_HOME/ghUpdateBranchWithUpstreamBranch.sh $UPSTREAM_BRANCH
