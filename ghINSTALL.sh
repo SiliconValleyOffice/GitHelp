f264e494569f10a29b7c8a3c25f3e3a212dd5d20
Steven-D-Stamps <Steven.D.Stamps@ESPN.com>
2017-12-04 18:19:34 -0500

    fix concatenating bash_profile

--- ghINSTALL.sh
+++ ghINSTALL.sh
@@ -5,10 +5,6 @@ printf "\nInstall GitHelp?\n"
 if [[ -d ~/GitHelp ]]; then
     printf "    Existing installation will be upgraded.\n"
     printf "    GitHelp repository configurations will be preserved.\n"
-    printf "    Note:\n"
-    printf "      If you are upgrading from a version that is earlier than 1.58\n"
-    printf "      you should manually delete the GitHelp lines from ~/.bash_profile\n"
-    printf "      before proceeding with this upgrade.\n"
 fi
 read -p "Are you sure?  (y/n)   " -n 1 -r
 echo
@@ -22,7 +18,7 @@ if [[ -a ~/.bash_profile ]]; then
     BACKUP_NAME=".bash_profile-"`date '+%Y-%m-%d-%R'`
     cp ~/.bash_profile ~/$BACKUP_NAME
     printf "\nYour current .bash_profile has been backed up to $BACKUP_NAME\n\n"
-    sed -i '/#=====  GitHelp - START  =====/,/#=====  GitHelp - END  =======/d' ~/.bash_profile
+    sed -i '/#=====  GitHelp - START  =====/,$d' ~/.bash_profile
     cat GitHelp/bash_profile >> ~/.bash_profile
 else
     cp GitHelp/bash_profile ~/.bash_profile
@@ -33,4 +29,4 @@ if [[ -d ~/GitHelp ]]; then
 fi
 cp -rf GitHelp ~/
 
-printf "WARNING:\n    Don't forget to update environment variables by running\n    source ~/.bash_profile\n\n"
+printf "WARNING:\n    Close all existing bash windows and reopen.\n\n"
