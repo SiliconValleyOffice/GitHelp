499bb2fd25bd14de9b615e294e12fc0d83b9018e
Steven-D-Stamps <Steven.D.Stamps@ESPN.com>
2017-12-05 18:10:30 -0500

    fix ghUBUD bug

--- GitHelp/ghNewPullRequestForDevelopment.sh
+++ GitHelp/ghNewPullRequestForDevelopment.sh
@@ -47,18 +47,6 @@ if [ $# -eq 1 ]; then
     fi
 else
     UPSTREAM_BRANCH="development"
-    CURRENT_BRANCH=`$GITHELP_HOME/ghCurrentBranchName.sh`
-    RESULTS=`git status`
-    if [[ "$RESULTS"  != *"working directory clean"* ]] && [[ "$RESULTS"  != *"working tree clean"* ]] ; then
-        printf "\nERROR:  \"${CURRENT_BRANCH}\" branch is not "clean".\n"
-        printf "    Commit/push your changes and try again.\n\n"
-        exit 1;
-    fi
-    if [[ "$RESULTS" = *"Your branch is ahead"* ]] ; then
-        printf "\nERROR:  You forgot to push your changes in branch \"${CURRENT_BRANCH}\".\n"
-        printf "    Run \"git push\" and try again.\n\n"
-        exit 1;
-    fi
 fi
 
 $GITHELP_HOME/ghUpstreamBranchExists.sh ${UPSTREAM_BRANCH} &> /dev/null
