ec6b94977b82e44af8c2a20c917a34b671ca0686
stampss <steven.d.stamps@espn.com>
2018-08-18 10:20:18 -0400

    make ghResetHard even safer

--- GitHelp/ghResetHard.sh
+++ GitHelp/ghResetHard.sh
@@ -13,4 +13,12 @@ then
     exit 1
 fi
 
+read -p "Are you REALLY sure?  (y/n)   " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    printf "\nOperation canceled.\n\n"
+    exit 1
+fi
+
 git reset --hard
