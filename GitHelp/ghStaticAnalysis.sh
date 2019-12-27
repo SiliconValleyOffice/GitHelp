847be70800da6a21b24fe1d1b1031707af7546e5
Steven D. Stamps <Steven.D.Stamps@Disney.com>
2019-12-27 12:56:02 -0500

    improve static analysis

--- GitHelp/ghStaticAnalysis.sh
+++ GitHelp/ghStaticAnalysis.sh
@@ -2,6 +2,7 @@
 # Static Analysis
 # alias = ghSA
 
+STATIC_RESULTS=0
 cd $GIT_ROOT
 
 ./gradlew tasks 2> /dev/null | grep detekt &> /dev/null
@@ -10,14 +11,36 @@ RUN_DETEKT=$?
 ./gradlew tasks 2> /dev/null | grep lintDebug &> /dev/null
 RUN_LINT_DEBUG=$?
 
-if [ "$RUN_DETEKT" -eq 0 -a "$RUN_LINT_DEBUG" -eq 0 ] ; then
-# if [ "$RUN_DETEKT" -eq 0 ] ; then
-    printf "\nStatic analysis...\n"
-    ./gradlew detekt lintDebug
-    STATIC_RESULTS=$?
+./gradlew tasks 2> /dev/null | grep jacocoTestReport &> /dev/null
+RUN_JACOCO=$?
+
+#if [ "$RUN_DETEKT" -eq 0 -a "$RUN_LINT_DEBUG" -eq 0 ] ; then
+if [ "$RUN_DETEKT" -eq 0 ] ; then
+    printf "\nStatic analysis - detekt\n"
+    ./gradlew detekt
+    DETEKT_RESULTS=$?
 else
-    printf "\nStatic analysis not enabled.\n\n"
-    STATIC_RESULTS=0
+    printf "\nDetekt not enabled.\n\n"
+    DETEKT_RESULTS=0
 fi
 
+if [ "$RUN_LINT_DEBUG" -eq 0 ] ; then
+    printf "\nStatic analysis - linkDebug\n"
+    ./gradlew lintDebug
+    LINT_DEBUG_RESULTS=$?
+else
+    printf "\nlintDebug not enabled.\n\n"
+    LINT_DEBUG_RESULTS=0
+fi
+
+if [ "$RUN_JACOCO" -eq 0 ] ; then
+    printf "\nStatic analysis - jacocoTestReport\n"
+    ./gradlew jacocoTestReport -Pkotlin.compiler.allWarningsAsErrors=true
+    JACOCO_RESULTS=$?
+else
+    printf "\njacocoTestReport not enabled.\n\n"
+    JACOCO_RESULTS=0
+fi
+
+STATIC_RESULTS=$((DETEKT_RESULTS+LINT_DEBUG_RESULTS+JACOCO_RESULTS))
 exit $STATIC_RESULTS
