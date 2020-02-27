44e1d642e03d4a61a26e76bd0c3dbc62d31e4851
Steven D. Stamps <Steven.D.Stamps@Disney.com>
2020-02-27 14:32:41 -0500

    tweaks

--- GitHelp/ghGitHubMergeRequestUrl.sh
+++ GitHelp/ghGitHubMergeRequestUrl.sh
@@ -11,5 +11,5 @@ CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 UPSTREAM_PROJECT=`git config --get remote.upstream.url | sed 's/git@//' | sed 's/com:/com\//' | sed 's/\.git//'`
 ORIGIN_PROJECT=`git config --get remote.origin.url | awk -F/ '{print $4 "/" $5}' | sed s/\.git//`
 
-echo "$UPSTREAM_PROJECT/compare/${UPSTREAM_BRANCH}...$ORIGIN_PROJECT?expand=1"
+echo "$UPSTREAM_PROJECT/compare/${UPSTREAM_BRANCH}...$ORIGIN_PROJECT:$CURRENT_BRANCH?expand=1"
 
