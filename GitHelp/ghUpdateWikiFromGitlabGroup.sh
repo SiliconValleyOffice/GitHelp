a2fce1d59957526c3530275bd4dcebb55107ab3d
Tan-JamickoJohn <jjt025@ucsd.edu>
2021-07-08 12:26:18 -0700

    repurposed ghUWFGG to update the wiki of the Gitlab Group

--- GitHelp/ghUpdateWikiFromGitlabGroup.sh
+++ GitHelp/ghUpdateWikiFromGitlabGroup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# Push latest changes to upstream wiki
-# alias = ghUW
-
-# Assumption - we are working with the wiki for the current githelp repo
-
+# Push latest changes to Gitlab group wiki
+# alias = ghUWFGG
+ 
 
 if [ "$#" -ne 1 ]; then
   printf "\nUsage: ghUW gitlab_group_name\n\n"
@@ -11,26 +9,18 @@ if [ "$#" -ne 1 ]; then
 fi
 
 GITLAB_GROUP_NAME="$1"
-LOCAL_PARENT_DIRECTORY=$(dirname "${GIT_ROOT}")
-BASE_REPO=${GIT_ROOT##*/}
-REPO_NAME="${BASE_REPO}.wiki"
-REPO_ROOT=${GIT_ROOT}.wiki
-WIKI_NAME="${GITLAB_GROUP_NAME}.wiki" 
+LOCAL_PARENT_DIRECTORY=$(dirname "${GIT_ROOT}") 
+WIKI_NAME="${GITLAB_GROUP_NAME}.wiki"  
+WIKI_ROOT="$LOCAL_PARENT_DIRECTORY/${GITLAB_GROUP_NAME}.wiki" 
 
-if [ ! -d $REPO_ROOT ]; then
-    printf "\n$REPO_ROOT does not exist.\nOperation canceled.\n\n"
-    printf "Use ghCW to clone the wiki for the current repo.\n\n"
-    exit 1
-fi
-
-if [ ! -d "$LOCAL_PARENT_DIRECTORY/$WIKI_NAME" ]; then
-    printf "\n$LOCAL_PARENT_DIRECTORY/$WIKI_NAMEWIKI_NAME does not exist.\nOperation canceled.\n\n"
+if [ ! -d "$WIKI_ROOT" ]; then
+    printf "\n$WIKI_ROOT does not exist.\nOperation canceled.\n\n"
     printf "Use ghCWFGG to clone the wiki for this Gitlab Group.\n\n"
     exit 1
 fi 
 
-printf "\nUpdate upstream WIKI for $BASE_REPO\n"
-printf "    with WIKI from Gitlab group ${GITLAB_GROUP_NAME} ?\n\n"
+printf "\nUpdate WIKI for Gitlab group ${GITLAB_GROUP_NAME}\n"
+printf "    with local updates?\n\n"
 read -p "Are you sure?  (y/n)   " -n 1 -r
 echo
 if [[ ! $REPLY =~ ^[Yy]$ ]]
@@ -39,12 +29,10 @@ then
     exit 1
 fi
 
-echo
-cd $LOCAL_PARENT_DIRECTORY 
-cp -rf $WIKI_NAME/* $REPO_ROOT
-cd "$REPO_ROOT"
+echo 
+cd "$WIKI_ROOT"
 git add -A
-LINES=`git status | grep "nothing to commit" | wc -l | cut -f1 -d' '`
+LINES=`git status | grep "nothing to commit" | wc -l`
 if [ $LINES != "0" ]; then
   printf "\nNo local changes to push\n\n"
   exit
